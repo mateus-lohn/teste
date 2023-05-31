daf9e2ace6294452374ee59b707c68715556519f
Mateus <mateusfln@gmail.com>
2023-05-31 01:02:24 -0300

    gozei no header

---
+++ src/Header/header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+
+    return(
+
+        <header className="App-header">
+            <div>
+            Home
+            </div>
+            <a className="App-a" href="https://instagram.com/lami_itm">
+            Perfil
+            </a>
+            
+        </header>
+        
+        
+
+    )
+
+}
+export default Header
