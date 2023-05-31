d213d045c165f4122d740105acf2f20fb38f7089
MateusLohn <mateus_lohn@estudante.sesisenai.org.br>
2023-05-31 01:35:52 -0300

    developing header for the website

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,9 +1,14 @@
+import logo from '../Header/teste.png'
+
 function Header(){
 
     return(
 
         <header className="App-header">
-            <div>
+            <img src={logo} alt='aaaa'
+            className='App-img'
+            />
+            <div className='App-a'>
             Home
             </div>
             <a className="App-a" href="https://instagram.com/lami_itm">
